24b83d7a45e7caa549d0073c2815d5aa355fe3a7
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 15:51:29 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid 
-  include Dance 
-  extend MetaDancing 
+  # include Dance 
+  # extend MetaDancing 
   
   attr_accessor :name 
   
