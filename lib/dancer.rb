af43650cafe302dc1b432fdb0b32c792b8e1411d
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 15:50:42 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,9 +1,11 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 class Dancer 
   # include Dance 
   # extend MetaDancing 
+  
   
   attr_accessor :name 
   
